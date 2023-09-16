1046eace047c61e4529e10a77ac508aa294cec35
MunirTheBest <msunchalyaev@gmail.com>
2023-09-16 19:37:35 +0300

    implements CommandLineRunner

--- src/main/java/com/munsun/spring_jdbc/app/SpringJdbcApplication.java
+++ src/main/java/com/munsun/spring_jdbc/app/SpringJdbcApplication.java
@@ -1,10 +1,20 @@
 package com.munsun.spring_jdbc.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJdbcApplication {
+public class SpringJdbcApplication implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println();
+        System.out.println("munsun " + "admin " + "ADMIN " + "ACTIVE");
+        System.out.println("kol2023 " + "12345 " + "USER " + "ACTIVE");
+        System.out.println("lololo " + "lololo " + "USER " + "REMOVED");
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(SpringJdbcApplication.class, args);
     }
